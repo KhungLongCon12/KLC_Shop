ad757ce539d6c46d0c303478c66c8738543a59cb
KhungLongCon12 <luonghung2782001@gmail.com>
2023-04-28 16:17:11 +0700

    commit

--- FrontEnd/src/modules/ImageSlider.jsx
+++ FrontEnd/src/modules/ImageSlider.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-import { Box } from "@mui/material"
+import React from 'react'
+import Carousel from 'react-material-ui-carousel'
+import imgSlider from "../helper/slider-img.json"
+import { Button, Paper } from '@mui/material'
+
 
 export function ImageSlider() {
     return (
-        <Box>
-
-
-        </Box>
-    );
+        <Carousel >
+            {
+                imgSlider.map(item => <Item key={item.id} item={item} />)
+            }
+        </Carousel>
+    )
 }
+
+function Item({ item }) {
+    return (
+        <Paper>
+            <img src={item.image} alt={item.description} style={{ width: '100%', height: '45vh' }} />
+            <h2>{item.description}</h2>
+            <Button>
+                Check it
+            </Button>
+        </Paper>
+
+    )
+}
+
+
+
--- FrontEnd/src/pages/HomePage.jsx
+++ FrontEnd/src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from '../modules/Navbar';
+import { ImageSlider } from '../modules/ImageSlider';
+
 
 
 export default function HomePage() {
     return (
         <React.Fragment>
             <Navbar />
+            <ImageSlider />
         </React.Fragment>
     )
 }
