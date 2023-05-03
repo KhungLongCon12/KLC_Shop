cb26b07cc4e4a763c1e8ecf4979237fc0402c8d3
KhungLongCon12 <luonghung2782001@gmail.com>
2023-05-03 13:41:11 +0700

    commit change logo and styles

--- FrontEnd/src/modules/Navbar.jsx
+++ FrontEnd/src/modules/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AppBar, Avatar, Badge, Box, Button, Card, Toolbar, Typography } from "@mui/material";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Stack } from "@mui/system";
-import logo from '../assets/img/dyno-store.png'
+import logo from '../assets/img/dynoStoreLogo2.png'
 import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
 import HowToRegTwoToneIcon from '@mui/icons-material/HowToRegTwoTone';
 import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
@@ -13,7 +13,7 @@ export default function Navbar() {
     const customTheme = createTheme({
         palette: {
             primary: {
-                main: '#DC678E',
+                main: '#011c37',
             }
         },
     })
@@ -31,8 +31,23 @@ export default function Navbar() {
                     <ThemeProvider theme={customTheme}>
                         <AppBar position="static" color="primary">
                             <Toolbar>
-                                <Avatar src={logo} alt='dyno-logo' sx={{ width: 60, height: 60, margin: 1 }} onDoubleClick={() => { navigate('/') }} />
-                                <Typography variant="h3" sx={{ paddingLeft: 1, fontFamily: 'cursive', color: '#80cc28' }}>
+                                <Avatar src={logo} alt='dyno-logo'
+                                    sx={{
+                                        '& .MuiAvatar-img': {
+                                            width: "200%",
+                                            height: "200%",
+                                            textAlign: "center",
+                                            objectFit: "cover",
+                                            color: "transparent",
+                                            textIndent: "10000px",
+                                        },
+                                        width: "80px",
+                                        height: "60px",
+                                        margin: 1,
+                                        border: "none"
+                                    }}
+                                    onDoubleClick={() => { navigate('/') }} />
+                                <Typography variant="h3" sx={{ paddingLeft: 1, fontFamily: 'cursive', color: '#a48c52' }}>
                                     Dyno Store Welcome !
                                 </Typography>
                                 <Stack flexGrow={1} spacing={1} direction='row' justifyContent="flex-end">
